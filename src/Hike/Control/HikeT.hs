e2a3079a5b96085dae53f138af323abd7523bb15
Jeremy Hughes <jedahu@gmail.com>
2014-09-17 17:43:45 +1200

    HikeT: try to restrict actions

--- src/Hike/Control/HikeT.hs
+++ src/Hike/Control/HikeT.hs
@@ -48,41 +48,76 @@ instance Applicative DState where
     (Diff f) <*> (Same x) = Diff (f x)
     (Diff f) <*> (Diff x) = Diff (f x)
 
-data CacheF a
+data CacheF m a
     = CacheWrite Text Dynamic a
     | CacheRead Text (Maybe Dynamic -> a)
+    | CacheAct (m a)
     deriving (Functor)
 
 type Cache = Map Text Dynamic
 
 type Act r e m = ReaderT r (ErrorT e m)
-type Require r s e m = FreeT CacheF (ReaderT r (StateT (s, Cache) (ErrorT e m)))
+type RunM r s e m = ReaderT r (StateT (s, Cache) (ErrorT e m))
+type Require r s e m = FreeT (CacheF m) (RunM r s e Identity)
 
 newtype Condition r s e m a =
-    Condition (Require r s e m (DState a))
+    Condition (FreeT (CacheF m) (RunM r s e m) (DState a))
     deriving (Functor)
 
 newtype Check r s e m a =
-    Check (Require r s e m (DState a))
+    Check (FreeT (CacheF m) (RunM r s e m) (DState a))
     deriving (Functor)
 
 data Task r s e m a =
-    Task (Require r s e m (DState a)) (Check r s e m a) Text
+    Task (FreeT (CacheF m) (RunM r s e m) (DState a)) (Check r s e m a) Text
     deriving (Functor)
 
-cacheWrite :: (Monad m) => Text -> Dynamic -> FreeT CacheF m ()
+instance (Error e, Applicative m, Monad m) => Applicative (Condition r s e m) where
+    pure = Condition . pure . pure
+
+    (Condition f) <*> (Condition a) = Condition (liftA2 (<*>) f a)
+
+papply :: IO (a -> b) -> IO a -> IO b
+papply f = join . liftA2 (<*>) (return f) . spawn
+
+plift2 :: (a -> b -> c) -> IO a -> IO b -> IO c
+plift2 f a b = return f `papply` a `papply` b
+
+(*|*) :: (Error e)
+      => Condition r s e IO (a -> b)
+      -> Condition r s e IO a
+      -> Condition r s e IO b
+(Condition f) *|* (Condition a) = undefined -- Condition apply
+--   where
+--     apply :: (Error e) => FreeT (CacheF IO) (RunM r s e IO) (DState b)
+--     apply = case (runFreeT f, runFreeT a) of
+--         (Pure f, Pure a)
+
+cacheWrite :: (Error e, Functor m, Monad m, Monad n)
+           => Text -> Dynamic -> FreeT (CacheF m) (RunM r s e n) ()
 cacheWrite k v = liftF (CacheWrite k v ())
 
-cacheRead :: (Monad m) => Text -> FreeT CacheF m (Maybe Dynamic)
+cacheRead :: (Error e, Functor m, Monad m, Monad n)
+          => Text -> FreeT (CacheF m) (RunM r s e n) (Maybe Dynamic)
 cacheRead k = liftF (CacheRead k id)
 
-liftAct :: (Error e, Functor m, Monad m) => Act r e m a -> Require r s e m a
-liftAct = addStateF . FreeT . fmap Pure
-  where
-    addState e = StateT $ \s -> fmap (, s) e
-    addStateF  = hoistFreeT (mapReaderT addState)
+cacheAct :: (Error e, Functor m, Monad m, Monad n)
+         => m a -> FreeT (CacheF m) (RunM r s e n) a
+cacheAct io = liftF (CacheAct io)
 
-cache :: (Typeable a, Monad m) => FreeT CacheF m a -> Text -> FreeT CacheF m a
+liftActF :: (Error e, Functor m, Monad m)
+         => Act r e m a -> Act r e (Require r s e m) a
+liftActF = mapReaderT (mapErrorT cacheAct)
+
+liftAct :: (Error e, Functor m, Monad m) => Act r e m a -> Require r s e m a
+liftAct fa = ask
+    >>= runErrorT . runReaderT (liftActF fa)
+    >>= either throwError return
+
+cache :: (Error e, Typeable a, Functor m, Monad m)
+      => FreeT (CacheF m) (RunM r s e m) (DState a)
+      -> Text
+      -> FreeT (CacheF m) (RunM r s e m) (DState a)
 cache m k = do
     mdyn <- cacheRead k
     maybe run return (mdyn >>= fromDynamic)
@@ -92,29 +127,29 @@ cache m k = do
         cacheWrite k (toDyn x)
         return x
 
-precond :: (Typeable a, Error e, Monad m)
+precond :: (Error e, Typeable a, Functor m, Monad m)
         => Require r s e m (DState a) -> Text -> Condition r s e m a
-precond m k = Condition (cache m k)
+precond m k = Condition (cache (idToM' m) k)
 
-postcond :: (Typeable a, Error e, Monad m)
+postcond :: (Error e, Typeable a, Functor m, Monad m)
          => Require r s e m (DState a) -> Text -> Check r s e m a
-postcond m k = Check (cache m k)
+postcond m k = Check (cache (idToM' m) k)
 
 actRequire :: (Error e, Functor m, Monad m)
-           => Require r s e m (DState a)
+           => FreeT (CacheF m) (RunM r s e m) (DState a)
            -> (a -> Act r e m b)
-           -> Require r s e m (DState b)
-           -> Require r s e m (DState b)
+           -> FreeT (CacheF m) (RunM r s e m) (DState b)
+           -> FreeT (CacheF m) (RunM r s e m) (DState b)
 actRequire before m after = do
     pre  <- before
     post <- after
     let both = (,) <$> pre <*> post
     case (,) <$> pre <*> post of
       Same (_, b) -> return (Same b)
-      Diff (a, _) -> Diff <$> liftAct (m a)
+      Diff (a, _) -> Diff <$> idToM' (liftAct (m a))
 
-taskRequire :: (Typeable a, Error e, Monad m)
-            => Task r s e m a -> Require r s e m (DState a)
+taskRequire :: (Error e, Typeable a, Functor m, Monad m)
+            => Task r s e m a -> FreeT (CacheF m) (RunM r s e m) (DState a)
 taskRequire (Task m (Check post) k) = cache cond k
   where
     cond = post >>= dstate (return . Same) (const m)
@@ -128,39 +163,56 @@ task :: (Typeable a, Error e, Functor m, Monad m)
 task (Condition dep) m check@(Check post) k =
     Task (actRequire dep m post) check k
 
-runM :: (MonadState (s, Cache) m) => FreeT CacheF m a -> m a
-runM fa = runFreeT fa >>= go
+idToM :: (Error e, Monad m) => RunM r s e Identity a -> RunM r s e m a
+idToM = mapReaderT (mapStateT (mapErrorT idM))
   where
+    idM (Identity a) = return a
+
+idToM' :: (Error e, Functor m, Monad m, Monad n)
+      => Require r s e m a -> FreeT (CacheF m) (RunM r s e n) a
+idToM' = hoistFreeT idToM
+
+runM :: (Error e, Functor m, Monad m) => Require r s e m a -> RunM r s e m a
+runM fa = runM' (idToM' fa)
+  where
+    runM' fa' = runFreeT fa' >>= go
     go (Pure a)                  =
         return a
     go (Free (CacheWrite k v a)) =
-        modify (\(x, y) -> (x, Map.insert k v y)) >> runM a
+        modify (\(x, y) -> (x, Map.insert k v y)) >> runM' a
     go (Free (CacheRead k f))    =
-        gets (Map.lookup k . snd) >>= runM . f
+        gets (Map.lookup k . snd) >>= runM' . f
+    go (Free (CacheAct m))       =
+        (lift . lift . lift) m >>= runM'
 
-puts :: (MonadIO m, Show a) => FreeT CacheF m a -> m a
-puts fa = runFreeT fa >>= go
+record :: (Error e, Show a, Functor m, Monad m)
+       => Require r s e m a -> RunM r s e (Writer (DList (Text, Text))) ()
+record fa = record' (idToM' fa >> return ())
   where
-    go (Pure a) = do
-        liftIO (putStrLn ("done " ++ show a))
-        return a
-    go (Free (CacheWrite k v a)) = do
-        liftIO (putStrLn ("write " ++ unpack k))
-        puts a
-    go (Free (CacheRead k f)) = do
-        liftIO (putStrLn ("read " ++ unpack k))
-        puts (f Nothing)
-
-record :: (MonadWriter (DList (Text, Text)) m, Show a)
-       => FreeT CacheF m a -> m a
-record fa = runFreeT fa >>= go
-  where
+    record' fa' = runFreeT fa' >>= go
     go (Pure a) = do
         tell (DList.singleton ("done", pack (show a)))
         return a
     go (Free (CacheWrite k v a)) = do
         tell (DList.singleton ("write", k))
-        record a
+        record' a
     go (Free (CacheRead k f)) = do
         tell (DList.singleton ("read", k))
-        record (f Nothing)
+        record' (f Nothing)
+    go (Free (CacheAct m)) = do
+        tell (DList.singleton ("act", ""))
+        record' (return ())
+
+-- puts :: (Error e, MonadIO m, Show a) => Require r s e m a -> RunM r s e IO a
+-- puts fa = runFreeT fa >>= go
+--   where
+--     go (Pure a) = do
+--         liftIO (putStrLn ("done " ++ show a))
+--         return a
+--     go (Free (CacheWrite k v a)) = do
+--         liftIO (putStrLn ("write " ++ unpack k))
+--         puts a
+--     go (Free (CacheRead k f)) = do
+--         liftIO (putStrLn ("read " ++ unpack k))
+--         puts (f Nothing)
+-- 
